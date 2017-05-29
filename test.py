4d4c6abb1dea842ad8faa5cb4d6d17c8da4cb422
feixh <hzhsfxh@gmail.com>
2017-05-29 15:47:42 -0700

    testing overlay func; need to handle boundary case

---
+++ test.py
@@ -0,0 +1,55 @@
+import cv2
+import numpy as np
+
+
+def overlay_image(foreground, background, rect):
+    """
+    overlay the foreground image on the background image
+    at the specified location
+    :param foreground: the foreground image, numpy array
+    :param background: the background image, numpy array
+    :param rect: a rectangle indicating where to overlay
+    :return: overlaid image
+    """
+    row, col = foreground.shape[0:2]
+    x, y, w, h = rect
+    xc, yc = x + w/2, y + h/2
+    ratio_x, ratio_y = w/float(col), h/float(row)
+    ratio = max(ratio_x, ratio_y)
+    row, col = int(ratio*row) & 0xfffe, int(ratio*col) & 0xfffe
+    resized = cv2.resize(foreground, dsize=(col, row))
+    ret = background.copy()
+    ret[yc-row/2:yc+row/2, xc-col/2:xc+col/2, :] = resized.copy()
+    return ret
+
+
+
+face = cv2.imread('smiling_face.jpg')
+face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture()
+for i in range(10):
+    if cap.open(i):
+        print 'camera {} launched'.format(i)
+        break
+key = None
+face_location = None
+while key != ord('x'):
+    status, image = cap.read()
+    assert status, 'failed to grab image from camera'
+    # convert color image to grayscale image
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    faces = face_detector.detectMultiScale(gray, 1.1, 10)
+    if len(faces) != 1:
+        # use the previous detection result
+        pass
+    else:
+        face_location = faces[0]
+    if face_location is not None:
+        x, y, w, h = face_location
+#         image = cv2.rectangle(image, (x,y), (x+w,y+h), (255,0,0), 2)
+        image = overlay_image(foreground=face, background=image, rect=face_location)
+    cv2.imshow('display', image)
+    key = cv2.waitKey(30)
+cv2.destroyWindow('display')
+cap.release()
